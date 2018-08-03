81e4145483a9674c0dc686f73428834b0f870c7f
Micha de Vries <mt.dev@hotmail.com>
2018-08-03 19:11:35 +0200

    language system update

---
+++ app/lang.php
@@ -0,0 +1,17 @@
+<?php
+  if (isset($_GET['update']) && isset($_GET['lang']) && !empty($_GET['lang'])) {
+    $lang = $_GET['lang'];
+    setcookie('lang', $lang, time() + (86400 * 365 * 20), "/"); // 86400 = 1 day, times 365 a year, and that times 20 years is the maximum supported seconds for a php cookie
+  } else {
+    include 'config.php';
+    $basepath = $config['basePath'];
+    if (isset($_COOKIE['lang'])) {
+      $lang_code = $_COOKIE['lang'];
+      $file = $basepath.'/assets/lang/'.$lang_code.'.json';
+    } else {
+      $file = $basepath.'/assets/lang/en.json';
+    }
+    $json = file_get_contents($file);
+    $lang = json_decode($json, true);
+  }
+?>
