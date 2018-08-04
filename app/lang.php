bf7eb79c1c49986b9594caaa6b909e2a51153f0a
Nathan Pipes <admin@example.com>
2018-08-04 22:50:11 +0800

    Fixed lang.

--- app/lang.php
+++ app/lang.php
@@ -1,17 +1,14 @@
 <?php
-  if (isset($_GET['update']) && isset($_GET['lang']) && !empty($_GET['lang'])) {
+if (isset($_GET['update']) && isset($_GET['lang']) && !empty($_GET['lang'])) {
     $lang = $_GET['lang'];
     setcookie('lang', $lang, time() + (86400 * 365 * 20), "/"); // 86400 = 1 day, times 365 a year, and that times 20 years is the maximum supported seconds for a php cookie
-  } else {
-    include 'config.php';
-    $basepath = $config['basePath'];
+} else {
     if (isset($_COOKIE['lang'])) {
-      $lang_code = $_COOKIE['lang'];
-      $file = $basepath.'/assets/lang/'.$lang_code.'.json';
+        $lang_code = $_COOKIE['lang'];
+        $file = 'assets/lang/' . $lang_code . '.json';
     } else {
-      $file = $basepath.'/assets/lang/en.json';
+        $file = 'assets/lang/en.json';
     }
     $json = file_get_contents($file);
     $lang = json_decode($json, true);
-  }
-?>
+}
